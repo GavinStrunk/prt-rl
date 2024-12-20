cf55cfcc33db8a590502e999ffff80660ced7997
Gavin Strunk <gavin.strunk@ssci.com>
2024-12-20 14:16:20 -0500

    implemented KeyboardPolicy
    the keyboard policy maps string values of pressed keys to discrete actions. The policy blocks on each simulation step to wait for the next key.

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 My code related to learning and using RL outside of a specific project
 
 ## To Do
-- implement random policy
-- unit test jhu wrapper
 - unit test decision functions
-- unit test policies
 - add executor that runs scenario from trained policies
 - connect mlflow
 - add sphinx docs
--- src/prt_rl/utils/policies.py
+++ src/prt_rl/utils/policies.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Dict
 
 import numpy as np
 import torch
@@ -76,6 +76,103 @@ class RandomPolicy:
 
         state['action'] = action
         return state
+
+
+class KeyboardPolicy:
+    """
+    The keyboard policy allows interactive control of the agent using keyboard input.
+
+    Args:
+        env_params (EnvParams): environment parameters
+        key_action_map (Dict[str, int]): mapping from key string to action value
+        blocking (bool): If blocking is True the simulation will wait for keyboard input at each step (synchronous), otherwise the simulation will not block and use the most up-to-date value (asynchronous). Default is True.
+
+    Example:
+        from prt_rl.utils.policies import KeyboardPolicy
+
+        policy = KeyboardPolicy(
+            env_params=env.get_parameters(),
+            key_action_map={
+                'up': 0,
+                'down': 1,
+                'left': 2,
+                'right': 3,
+            },
+            blocking=True
+        )
+
+        action_td = policy.get_action(state_td)
+
+    """
+    def __init__(self,
+                 env_params: EnvParams,
+                 key_action_map: Dict[str, int],
+                 blocking: bool = True,
+                 ) -> None:
+        # Check if pynput is installed
+        try:
+            from pynput import keyboard
+        except ImportError as e:
+            raise ImportError(
+                "The 'pynput' library is required for KeyboardPolicy but is not installed. "
+                "Please install it using 'pip install pynput'."
+            ) from e
+        self.keyboard = keyboard
+
+        self.env_params = env_params
+        self.key_action_map = key_action_map
+        self.blocking = blocking
+
+    def get_action(self,
+                   state: TensorDict
+                   ) -> TensorDict:
+        """
+        Gets a keyboard press and maps it to the action space.
+        """
+        assert state.batch_size[0] == 1, "KeyboardPolicy Only supports batch size 1 for now."
+
+        if self.blocking:
+            key_string = ''
+            # Keep reading keys until a valid key in the map is received
+            while key_string not in self.key_action_map:
+                key_string = self._wait_for_key_press()
+            action_val = self.key_action_map[key_string]
+        else:
+            raise NotImplementedError("Only blocking actions are currently supported in the keyboard policy.")
+
+        state['action'] = torch.tensor([[action_val]])
+        return state
+
+    def _wait_for_key_press(self) -> str:
+        """
+        Blocking method to wait for keyboard press.
+
+        Returns:
+            str: String name of the pressed key
+        """
+        # A callback function to handle key presses
+        def on_press(key):
+            nonlocal key_pressed
+            key_pressed = key  # Store the pressed key
+            return False  # Stop the listener after a key is pressed
+
+        key_pressed = None
+        # Start the listener in blocking mode
+        # Supressing keys keeps them from being passed on to the rest of the computer
+        with self.keyboard.Listener(on_press=on_press, suppress=True) as listener:
+            listener.join()
+
+        # Get string value of KeyCodes and special Keys
+        if isinstance(key_pressed, self.keyboard.KeyCode):
+            key_pressed = key_pressed.char
+        elif isinstance(key_pressed, self.keyboard.Key):
+            key_pressed = key_pressed.name
+        else:
+            raise ValueError(f"Unrecognized key pressed type: {type(key_pressed)}")
+
+        return key_pressed
+
+
 
 
 class QTablePolicy(Policy):
