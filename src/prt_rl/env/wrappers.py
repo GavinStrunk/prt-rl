06b27204dfa994e61dae89f10bb499cd217976b0
Gavin Strunk <gavin.strunk@ssci.com>
2025-03-13 15:38:06 -0400

    disabled prt_sim tests

--- src/prt_rl/env/wrappers.py
+++ src/prt_rl/env/wrappers.py
@@ -5,77 +5,77 @@ from tensordict.tensordict import TensorDict
 import torch
 from typing import Optional, Tuple, List, Union
 import vmas
-from prt_sim.jhu.base import BaseEnvironment
-from prt_sim.jhu.bandits import KArmBandits
+# from prt_sim.jhu.base import BaseEnvironment
+# from prt_sim.jhu.bandits import KArmBandits
 from prt_rl.env.interface import EnvironmentInterface, EnvParams, MultiAgentEnvParams, MultiGroupEnvParams
 
 
-class JhuWrapper(EnvironmentInterface):
-    """
-    Wraps the JHU environments in the Environment interface.
+# class JhuWrapper(EnvironmentInterface):
+#     """
+#     Wraps the JHU environments in the Environment interface.
 
-    The JHU environments are games and puzzles that were used in the JHU 705.741 RL course.
-    """
+#     The JHU environments are games and puzzles that were used in the JHU 705.741 RL course.
+#     """
 
-    def __init__(self,
-                 environment: BaseEnvironment,
-                 render_mode: Optional[str] = None,
-                 ) -> None:
-        super().__init__(render_mode)
-        self.env = environment
+#     def __init__(self,
+#                  environment: BaseEnvironment,
+#                  render_mode: Optional[str] = None,
+#                  ) -> None:
+#         super().__init__(render_mode)
+#         self.env = environment
 
-    def get_parameters(self) -> EnvParams:
-        params = EnvParams(
-            action_len=1,
-            action_continuous=False,
-            action_min=0,
-            action_max=self.env.get_number_of_actions() - 1,
-            observation_shape=(1,),
-            observation_continuous=False,
-            observation_min=0,
-            observation_max=max(self.env.get_number_of_states() - 1, 0),
-        )
-        return params
+#     def get_parameters(self) -> EnvParams:
+#         params = EnvParams(
+#             action_len=1,
+#             action_continuous=False,
+#             action_min=0,
+#             action_max=self.env.get_number_of_actions() - 1,
+#             observation_shape=(1,),
+#             observation_continuous=False,
+#             observation_min=0,
+#             observation_max=max(self.env.get_number_of_states() - 1, 0),
+#         )
+#         return params
 
-    def reset(self) -> TensorDict:
-        state = self.env.reset()
-        state_td = TensorDict(
-            {
-                'observation': torch.tensor([[state]], dtype=torch.int),
-            },
-            batch_size=torch.Size([1])
-        )
+#     def reset(self) -> TensorDict:
+#         state = self.env.reset()
+#         state_td = TensorDict(
+#             {
+#                 'observation': torch.tensor([[state]], dtype=torch.int),
+#             },
+#             batch_size=torch.Size([1])
+#         )
 
-        # Add info for Bandit environment
-        if isinstance(self.env, KArmBandits):
-            state_td['info'] = {
-                'optimal_bandit': torch.tensor([[self.env.get_optimal_bandit()]], dtype=torch.int),
-            }
+#         # Add info for Bandit environment
+#         if isinstance(self.env, KArmBandits):
+#             state_td['info'] = {
+#                 'optimal_bandit': torch.tensor([[self.env.get_optimal_bandit()]], dtype=torch.int),
+#             }
 
-        if self.render_mode == 'human':
-            self.env.render()
-        elif self.render_mode == 'rgb_array':
-            rgb = self.env.render()
-            state_td['rgb_array'] = torch.tensor(rgb).unsqueeze(0)
+#         if self.render_mode == 'human':
+#             self.env.render()
+#         elif self.render_mode == 'rgb_array':
+#             rgb = self.env.render()
+#             state_td['rgb_array'] = torch.tensor(rgb).unsqueeze(0)
 
-        return state_td
+#         return state_td
 
-    def step(self, action: TensorDict) -> TensorDict:
-        action_val = action['action'][0].item()
-        state, reward, done = self.env.execute_action(action_val)
-        action['next'] = {
-            'observation': torch.tensor([[state]], dtype=torch.int),
-            'reward': torch.tensor([[reward]], dtype=torch.float),
-            'done': torch.tensor([[done]], dtype=torch.bool),
-        }
+#     def step(self, action: TensorDict) -> TensorDict:
+#         action_val = action['action'][0].item()
+#         state, reward, done = self.env.execute_action(action_val)
+#         action['next'] = {
+#             'observation': torch.tensor([[state]], dtype=torch.int),
+#             'reward': torch.tensor([[reward]], dtype=torch.float),
+#             'done': torch.tensor([[done]], dtype=torch.bool),
+#         }
 
-        if self.render_mode == 'human':
-            self.env.render()
-        elif self.render_mode == 'rgb_array':
-            rgb = self.env.render()
-            action['next', 'rgb_array'] = torch.tensor(rgb).unsqueeze(0)
+#         if self.render_mode == 'human':
+#             self.env.render()
+#         elif self.render_mode == 'rgb_array':
+#             rgb = self.env.render()
+#             action['next', 'rgb_array'] = torch.tensor(rgb).unsqueeze(0)
 
-        return action
+#         return action
 
 
 class GymnasiumWrapper(EnvironmentInterface):
--- tests/prt_rl/env/test_wrappers.py
+++ tests/prt_rl/env/test_wrappers.py
@@ -4,54 +4,54 @@ import torch
 import vmas
 from prt_rl.env import wrappers
 from prt_rl.env.interface import MultiAgentEnvParams, MultiGroupEnvParams
-from prt_sim.jhu.bandits import KArmBandits
-from prt_sim.jhu.robot_game import RobotGame
+# from prt_sim.jhu.bandits import KArmBandits
+# from prt_sim.jhu.robot_game import RobotGame
 
-def test_jhu_wrapper_for_bandits():
-    env = wrappers.JhuWrapper(environment=KArmBandits())
+# def test_jhu_wrapper_for_bandits():
+#     env = wrappers.JhuWrapper(environment=KArmBandits())
 
-    # Check the EnvParams are filled out correctly
-    params = env.get_parameters()
-    assert params.action_len == 1
-    assert params.action_continuous == False
-    assert params.action_min == 0
-    assert params.action_max == 9
-    assert params.observation_shape == (1,)
-    assert params.observation_continuous == False
-    assert params.observation_min == 0
-    assert params.observation_max == 0
+#     # Check the EnvParams are filled out correctly
+#     params = env.get_parameters()
+#     assert params.action_len == 1
+#     assert params.action_continuous == False
+#     assert params.action_min == 0
+#     assert params.action_max == 9
+#     assert params.observation_shape == (1,)
+#     assert params.observation_continuous == False
+#     assert params.observation_min == 0
+#     assert params.observation_max == 0
 
-    # Check interface
-    state_td = env.reset()
-    assert state_td.shape == (1,)
-    assert state_td['observation'].shape == (1, *params.observation_shape)
+#     # Check interface
+#     state_td = env.reset()
+#     assert state_td.shape == (1,)
+#     assert state_td['observation'].shape == (1, *params.observation_shape)
 
-    action = state_td
-    action['action'] = torch.tensor([[0]])
-    trajectory_td = env.step(action)
+#     action = state_td
+#     action['action'] = torch.tensor([[0]])
+#     trajectory_td = env.step(action)
 
-def test_jhu_wrapper_for_robot_game():
-    env = wrappers.JhuWrapper(environment=RobotGame())
+# def test_jhu_wrapper_for_robot_game():
+#     env = wrappers.JhuWrapper(environment=RobotGame())
 
-    # Check the EnvParams are filled out correctly
-    params = env.get_parameters()
-    assert params.action_len == 1
-    assert params.action_continuous == False
-    assert params.action_min == 0
-    assert params.action_max == 3
-    assert params.observation_shape == (1,)
-    assert params.observation_continuous == False
-    assert params.observation_min == 0
-    assert params.observation_max == 10
+#     # Check the EnvParams are filled out correctly
+#     params = env.get_parameters()
+#     assert params.action_len == 1
+#     assert params.action_continuous == False
+#     assert params.action_min == 0
+#     assert params.action_max == 3
+#     assert params.observation_shape == (1,)
+#     assert params.observation_continuous == False
+#     assert params.observation_min == 0
+#     assert params.observation_max == 10
 
-    # Check interface
-    state_td = env.reset()
-    assert state_td.shape == (1,)
-    assert state_td['observation'].shape == (1, *params.observation_shape)
+#     # Check interface
+#     state_td = env.reset()
+#     assert state_td.shape == (1,)
+#     assert state_td['observation'].shape == (1, *params.observation_shape)
 
-    action = state_td
-    action['action'] = torch.tensor([[0]])
-    trajectory_td = env.step(action)
+#     action = state_td
+#     action['action'] = torch.tensor([[0]])
+#     trajectory_td = env.step(action)
 
 def test_gymnasium_wrapper_for_cliff_walking():
     # Reference: https://gymnasium.farama.org/environments/toy_text/cliff_walking/
