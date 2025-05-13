b00be1b1317502a3fc22418398098aa9ac11d774
Gavin Strunk <gavin.strunk@gmail.com>
2025-05-13 05:46:19 -0400

    fixed broken unit test

--- tests/common/test_replay_buffers.py
+++ tests/common/test_replay_buffers.py
@@ -190,7 +190,7 @@ def test_update_priorities(simple_transition):
     td_errors = torch.randn(4)
     buffer.update_priorities(batch['indices'], td_errors)
     batch2 = buffer.sample(batch_size=4)
-    assert not torch.allclose(old_weights, batch2['weights'], atol=1e-6)
+    assert not torch.allclose(old_weights, batch2['weights'], atol=1e-4)
 
 def test_clear(simple_transition):
     buffer = PrioritizedReplayBuffer(capacity=10)
