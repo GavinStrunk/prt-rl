2195510c936ae757340261e6c69aef8a38c351c6
BumpVersion Action <bumpversion@github-actions>
2025-10-14 18:38:24 +0000

    Bump version: 0.5.3 → 0.5.4

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../demos'))
 project = 'Python Research Toolkit - Reinforcement Learning'
 copyright = '2024, Gavin Strunk'
 author = 'Gavin Strunk'
-version = "0.5.3"
+version = "0.5.4"
 release = version
 
 # -- General configuration ---------------------------------------------------
