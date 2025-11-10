44823c2bb27d4d8210dc6be29e1029a496264664
Gavin Strunk <gavin.strunk@gmail.com>
2025-11-10 15:48:11 -0500

    added in ppo phase 1 training

--- src/prt_rl/adaptation/rma.py
+++ src/prt_rl/adaptation/rma.py
@@ -66,6 +66,11 @@ class RMAPolicy(BasePolicy):
             env_params=env_params,
             critic_head_kwargs={'network_arch': [128, 128]},
             )
+        
+        self.adaptation_module = AdaptationModule(
+            env_dim=(self.env_params.observation_shape[0] + self.env_params.action_shape[0]) * self.time_steps,
+            extrinsics_dim=self.extrinsics_dim
+        )       
 
         env_dim = (env_params.observation_shape[0] + env_params.action_shape[0]) * time_steps
 
@@ -156,20 +161,6 @@ class RMA(BaseAgent):
         self.agent = agent
         self.device = device
 
-        # self.env_encoder = EnvironmentEncoder(
-        #     env_dim=(self.policy.env_params.observation_shape[0] + self.policy.env_params.action_shape[0]) * self.policy.time_steps,
-        #     extrinsics_dim=self.policy.extrinsics_dim
-        # ).to(device)
-
-        self.adaptation_module = AdaptationModule(
-            env_dim=(self.policy.env_params.observation_shape[0] + self.policy.env_params.action_shape[0]) * self.policy.time_steps,
-            extrinsics_dim=self.policy.extrinsics_dim
-        ).to(device)
-
-        # Construct optimizers
-        self.policy_optimizer = torch.optim.Adam(self.policy.parameters(), lr=self.config.learning_rate)
-        self.adaptation_optimizer = torch.optim.Adam(self.adaptation_module.parameters(), lr=self.config.adapt_learning_rate)
-
     def predict(self, 
                 state: torch.Tensor, 
                 deterministic: bool = False
@@ -206,15 +197,136 @@ class RMA(BaseAgent):
             evaluator (Evaluator | None): Evaluator for periodic evaluation during training.
             show_progress (bool): If True, display a progress bar during training.
         """
-        # Use the default PPO agent traing loop for Phase 1
-        self.agent.train(
-            env=env,
-            total_steps=total_steps,
-            schedulers=schedulers,
-            logger=logger,
-            evaluator=evaluator,
-            show_progress=show_progress
-        )
+
+        # Construct the environment (extrinsics) encoder
+        env_encoder = EnvironmentEncoder(
+            env_dim=(self.policy.env_params.observation_shape[0] + self.policy.env_params.action_shape[0]) * self.policy.time_steps,
+            extrinsics_dim=self.policy.extrinsics_dim
+        ).to(self.device)
+
+        actor_optimizer = torch.optim.Adam(self.policy.actor.parameters(), lr=self.config.learning_rate)
+        critic_optimizer = torch.optim.Adam([self.policy.critic.parameters(), env_encoder.parameters()], lr=self.config.learning_rate)
+
+        # Use PPO Training with Extrinsics Encoder + Policy
+        logger = logger or Logger.create('blank')
+
+        if show_progress:
+            progress_bar = ProgressBar(total_steps=total_steps)
+
+        num_steps = 0
+
+        # Make collector and do not flatten the experience so the shape is (N, T, ...)
+        collector = ParallelCollector(env=env, logger=logger, flatten=False)
+        rollout_buffer = RolloutBuffer(capacity=self.config.steps_per_batch, device=self.device)
+
+        while num_steps < total_steps:
+            # Update Schedulers if provided
+            if schedulers is not None:
+                for scheduler in schedulers:
+                    scheduler.update(current_step=num_steps)
+
+            # Collect experience dictionary with shape (N, T, ...)
+            experience = collector.collect_experience(policy=self.policy, num_steps=self.config.steps_per_batch)
+
+            # Compute Advantages and Returns under the current policy
+            advantages, returns = utils.generalized_advantage_estimates(
+                rewards=experience['reward'],
+                values=experience['value_est'],
+                dones=experience['done'],
+                last_values=experience['last_value_est'],
+                gamma=self.config.gamma,
+                gae_lambda=self.config.gae_lambda
+            )
+            
+            if self.config.normalize_advantages:
+                advantages = utils.normalize_advantages(advantages)
+
+            experience['advantages'] = advantages.detach()
+            experience['returns'] = returns.detach()
+
+            # Flatten the experience batch (N, T, ...) -> (N*T, ...) and remove the last_value_est key because we don't need it anymore
+            experience = {k: v.reshape(-1, *v.shape[2:]) for k, v in experience.items() if k != 'last_value_est'}
+            num_steps += experience['state'].shape[0]
+
+            # Add experience to the rollout buffer
+            rollout_buffer.add(experience)   
+
+            # Optimization Loop
+            clip_losses = []
+            entropy_losses = []
+            value_losses = []
+            losses = []
+            for _ in range(self.config.num_optim_steps):
+                for batch in rollout_buffer.get_batches(batch_size=self.config.mini_batch_size):
+
+                    # Optimize the critic + env encoder
+                    extrinsic_features = env_encoder(batch['state'])
+                    new_value_est = self.policy.critic(extrinsic_features)
+
+                    value_loss = F.mse_loss(new_value_est, batch['returns'])
+                    critic_optimizer.zero_grad()
+                    value_loss.backward()
+                    critic_optimizer.step()
+
+                    value_losses.append(value_loss.item())
+
+                    # Optimize the actor
+                    with torch.no_grad():
+                        extrinsic_features = env_encoder(batch['state'])
+                    
+                    new_log_probs, entropy = self.policy.actor.evaluate_actions(extrinsic_features, batch['action'])
+                    # new_value_est, new_log_prob, entropy = self.policy.evaluate_actions(batch['state'], batch['action'])
+                    
+                    # We detach the old log probs so we only computed gradients for the current policies parameters
+                    old_log_prob = batch['log_prob'].detach()
+
+                    # Ratio between new and old policy
+                    ratio = torch.exp(new_log_probs - old_log_prob)
+
+                    # Clipped surrogate loss
+                    batch_advantages = batch['advantages']
+                    clip_loss = batch_advantages * ratio
+                    clip_loss2 = batch_advantages * torch.clamp(ratio, 1 - self.config.epsilon, 1 + self.config.epsilon)
+                    clip_loss = -torch.min(clip_loss, clip_loss2).mean()
+
+                    entropy_loss = -entropy.mean()
+
+                    loss = clip_loss + self.config.entropy_coef*entropy_loss
+                    
+                    clip_losses.append(clip_loss.item())
+                    entropy_losses.append(entropy_loss.item())
+                    losses.append(loss.item())
+
+                    # Optimize
+                    actor_optimizer.zero_grad()
+                    loss.backward()
+                    # torch.nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
+                    actor_optimizer.step()
+
+            # Clear the buffer after optimization
+            rollout_buffer.clear()
+
+            # Update progress bar
+            if show_progress:
+                tracker = collector.get_metric_tracker()
+                progress_bar.update(current_step=num_steps, desc=f"Episode Reward: {tracker.last_episode_reward:.2f}, "
+                                                                   f"Episode Length: {tracker.last_episode_length}, "
+                                                                   f"Loss: {np.mean(losses):.4f},")
+            # Log metrics
+            if logger.should_log(num_steps):
+                logger.log_scalar('clip_loss', np.mean(clip_losses), num_steps)
+                logger.log_scalar('entropy_loss', np.mean(entropy_losses), num_steps)
+                logger.log_scalar('value_loss', np.mean(value_losses), num_steps)
+                logger.log_scalar('loss', np.mean(losses), num_steps)
+                # logger.log_scalar('episode_reward', collector.previous_episode_reward, num_steps)
+                # logger.log_scalar('episode_length', collector.previous_episode_length, num_steps)
+
+            if evaluator is not None:
+                # Evaluate the agent periodically
+                evaluator.evaluate(agent=self.policy, iteration=num_steps)
+
+        if evaluator is not None:
+            evaluator.close()                     
 
     def train_adaptation(self,
               env: EnvironmentInterface,
@@ -227,6 +339,9 @@ class RMA(BaseAgent):
 
         DAgger style supervised learning training.
         """
+
+        adaptation_optimizer = torch.optim.Adam(self.policy.adaptation_module.parameters(), lr=self.config.adapt_learning_rate)
+
         # Create collector and replay buffer
 
         num_steps = 0
